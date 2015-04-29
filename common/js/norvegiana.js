26bd5e5f8f342b665636045bcb79e7302eacffd6
Atle Frenvik Sveen <atle.sveen@norkart.no>
2015-04-29 10:08:14 +0200

    style features

--- common/js/norvegiana.js
+++ common/js/norvegiana.js
@@ -3,6 +3,16 @@
 var NorvegianaAPI = function () {
     'use strict';
 
+    var datasets = {
+        'Artsdatabanken': 'Artsdatabanken',
+        'difo': 'Digitalt fortalt',
+        'DiMu': 'DigitaltMuseum',
+        'Industrimuseum': 'Industrimuseum',
+        'Kulturminnesøk': 'Kulturminnesøk',
+        'Universitetsmuseene': 'MUSIT',
+        'Naturbase': 'Naturbase',
+        'Stedsnavn': 'Stedsnavn'
+    };
 
     var baseUrl = 'http://kulturnett2.delving.org/api/search'
 
@@ -68,16 +78,7 @@ var NorvegianaAPI = function () {
     }
 
 
-    function getWithin(query, latLng, distance, callback) {
-        distance = distance || 5000;
-        distance = distance / 1000; // convert to km
-        var params = {
-            query: query,
-            pt: formatLatLng(latLng),
-            d: distance,
-            format: 'json'
-        };
-        var url = baseUrl + '?'  + createQueryParameterString(params);
+    function sendRequest(url, callback) {
         $.ajax({
             type: 'get',
             url: url,
@@ -87,8 +88,79 @@ var NorvegianaAPI = function () {
         });
     }
 
+    function getWithin(query, latLng, distance, callback) {
+        distance = distance || 5000;
+        distance = distance / 1000; // convert to km
+        var params = {
+            query: query,
+            pt: formatLatLng(latLng),
+            d: distance,
+            format: 'json',
+            rows: 100
+        };
+        var url = baseUrl + '?'  + createQueryParameterString(params);
+        sendRequest(url, callback);
+    }
+
+    function getBbox(query, bbox, callback) {
+        //TODO: this does not work!
+        var params = {
+            query: query,
+            bbox: bbox,
+            geoType: 'bbox',
+            format: 'json'
+        };
+        var url = baseUrl + '?'  + createQueryParameterString(params);
+        sendRequest(url, callback);
+    }
+
+    var providerColors = {
+        'Artsdatabanken': 'darkpuple',
+        'Digitalt fortalt': 'orange',
+        'DigitaltMuseum': 'cadetblue',
+        'Industrimuseum': 'darkred',
+        'Kulturminnesøk': 'green',
+        'Naturbase': 'purple',
+        'Sentralt stedsnavnregister': 'darkgreen'
+    };
+
+    var contentIcons = {
+        'IMAGE': 'file-image-o',
+        'VIDEO': 'file-video-o',
+        'SOUND': 'music',
+        'TEXT': 'file-text'
+    };
+    
+
+    function styleFunction(feature, latlng) {
+
+        var provider = feature.properties.abm_contentProvider;
+        var color = 'blue';
+        if (_.has(providerColors, provider)) {
+            color = providerColors[provider];
+        }
+
+        var contentType = feature.properties.europeana_type;
+        var icon = 'file-o';
+        if (_.has(contentIcons, contentType)) {
+            icon = contentIcons[contentType];
+        }
+
+        var icon = L.AwesomeMarkers.icon({
+            icon: icon,
+            markerColor: color,
+            prefix: 'fa'
+        });
+
+        return L.marker(latlng,{
+            icon: icon
+        });
+    }
 
     return {
-        getWithin: getWithin
+        getWithin: getWithin,
+        getBbox: getBbox,
+        datasets: function () {return _.extend({}, datasets);},
+        styleFunction: styleFunction
     };
 };
