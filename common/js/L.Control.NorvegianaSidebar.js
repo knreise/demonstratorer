b4e89be67967436b52e2d545abbdc5e7e832b604
Atle Frenvik Sveen <atle.sveen@norkart.no>
2015-05-08 13:31:38 +0200

    add prev/next buttons, styling missing

--- common/js/L.Control.NorvegianaSidebar.js
+++ common/js/L.Control.NorvegianaSidebar.js
@@ -9,7 +9,7 @@ L.Control.NorvegianaSidebar = L.Control.Sidebar.extend({
         return L.Control.Sidebar.prototype.initialize.call(this, placeholder, options);
     },
 
-    showFeature: function (feature, template, getData, showList) {
+    showFeature: function (feature, template, getData, callbacks) {
 
         if (getData) {
             var self = this;
@@ -26,16 +26,28 @@ L.Control.NorvegianaSidebar = L.Control.Sidebar.extend({
         }
         var content = template(_.extend({image: null}, feature.properties));
 
-        if (showList) {
+        if (callbacks && callbacks.close) {
             L.DomEvent.off(this._closeButton, 'click', this.hide, this);
             L.DomEvent.on(this._closeButton, 'click', function (e) {
                 L.DomEvent.stopPropagation(e);
                 L.DomEvent.on(this._closeButton, 'click', this.hide, this);
-                showList();
+                callbacks.close();
             }, this);
         }
-
         this.setContent(content);
+
+        if (callbacks && callbacks.prev) {
+            //content += '<a href="#">prev</a>';
+            var prev = L.DomUtil.create('a', '', this.getContainer());
+            prev.innerHTML = 'prev';
+            L.DomEvent.on(prev, 'click', callbacks.prev);
+
+        }
+        if (callbacks && callbacks.next) {
+            var next = L.DomUtil.create('a', '', this.getContainer());
+            next.innerHTML = 'next';
+            L.DomEvent.on(next, 'click', callbacks.next);
+        }
 
         if (typeof audiojs !== 'undefined') {
             audiojs.createAll();
@@ -43,26 +55,59 @@ L.Control.NorvegianaSidebar = L.Control.Sidebar.extend({
         this.show();
     },
 
+    _createListCallbacks: function (feature, index, template, getData, features) {
+        var prev;
+        if (index > 0) {
+            prev = _.bind(function (e) {
+                e.preventDefault();
+                index = index - 1;
+                feature = features[index];
+                var callbacks = this._createListCallbacks(feature, index, template, getData, features);
+                this.showFeature(feature, template, getData, callbacks);
+            }, this);
+        }
+        var next;
+        if (index < features.length - 1) {
+            next = _.bind(function (e) {
+                e.preventDefault();
+                index = index + 1;
+                feature = features[index];
+                var callbacks = this._createListCallbacks(feature, index, template, getData, features);
+                this.showFeature(feature, template, getData, callbacks);
+            }, this);
+        }
+
+        return {
+            prev: prev,
+            close: _.bind(function () {
+                this.showFeatures(features, template, getData);
+            }, this),
+            next: next
+        };
+    },
+
+    _createListElement: function (feature, index, template, getData, features) {
+        var icon = KR.Util.iconForFeature(feature);
+        var li = $(this.options.listElementTemplate({
+            title: feature.properties.title,
+            icon: icon
+        }));
+
+        li.on('click', _.bind(function (e) {
+            e.preventDefault();
+            var callbacks = this._createListCallbacks(feature, index, template, getData, features);
+
+            this.showFeature(feature, template, getData, callbacks);
+            return false;
+        }, this));
+        return li;
+    },
+
     showFeatures: function (features, template, getData) {
         var list = $('<div class="list-group"></ul>');
-        var elements = _.map(features, function (feature) {
-
-            var icon = KR.Util.iconForFeature(feature);
-            var li = $(this.options.listElementTemplate({
-                title: feature.properties.title,
-                icon: icon
-            }));
-            li.on('click', _.bind(function (e) {
-                e.preventDefault();
-                this.showFeature(feature, template, getData, _.bind(function () {
-                    console.log("show list");
-                    this.showFeatures(features, template, getData);
-                }, this));
-                return false;
-            }, this));
-            return li;
+        var elements = _.map(features, function (feature, index) {
+            return this._createListElement(feature, index, template, getData, features);
         }, this);
-
         list.append(elements);
         $(this.getContainer()).html(list);
         this.show();
