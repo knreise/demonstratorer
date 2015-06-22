f968aaadeebc59a11daebe3da384a5448e199e7e
Atle Frenvik Sveen <atle.sveen@norkart.no>
2015-06-22 13:45:35 +0200

    fix generator page

--- demonstrator_content/generator/inline.js
+++ demonstrator_content/generator/inline.js
@@ -1,4 +1,3 @@
-var municipalityTemplate = _.template('<option value="<%= komm %>"><%= navn %></option>');
 var layerTemplate = _.template('<option value="<%= id %>" <% if(selected) {print("selected") } %>><%= id %></option>');
 var datasetTemplate = _.template(
     '<div class="checkbox">' +
@@ -16,7 +15,73 @@ var api = new KR.API({
     }
 });
 
-function buildMunicipalityList(element) {
+
+function buildLimitSelections(ids, municipalities) {
+    var callback;
+    var selected = 0;
+
+    var extras = {
+        'line': {
+            allstatic: true
+        }
+    };
+
+    var elements = _.map(ids, function (id, idx) {
+        var template = _.template($('#' + id + '_template').html());
+        var element = $('#' + id + '_container').html(template({
+            municipalities: municipalities
+        }));
+        if (idx !== selected) {
+            element.find('select, input').attr('disabled', 'disabled');
+        }
+        return element;
+    });
+
+    _.each(elements, function (element, index) {
+        element.on('click', function () {
+            _.each(elements, function (e, idx) {
+                if (idx === index) {
+                    elements[idx].find('select, input').removeAttr('disabled');
+                    selected = index;
+                } else {
+                    elements[idx].find('select, input').attr('disabled', 'disabled');
+                }
+                if (callback) {
+                    callback();
+                }
+            });
+        });
+    });
+
+    _.each(elements, function (element) {
+        element.find('select, input').on('change', function (e) {
+            if (callback) {
+                callback();
+            }
+        });
+    });
+
+    return {
+        getValues: function () {
+            var values = {};
+            if (_.has(extras, ids[selected])) {
+                values = extras[ids[selected]];
+            }
+            return _.reduce(elements[selected].find('select, input'), function (acc, el) {
+                acc[el.id] = $(el).val();
+                return acc;
+            }, values);
+        },
+        callback: function (cb) {
+            callback = cb;
+            cb();
+        }
+    };
+
+}
+
+
+function getMunicipalityList(callback) {
     var dataset = {
         api: 'cartodb',
         query: 'SELECT komm, navn FROM kommuner ORDER BY navn',
@@ -25,19 +90,12 @@ function buildMunicipalityList(element) {
         }
     };
 
-    api.getData(dataset, function (response) {
-        element.html(_.map(response, municipalityTemplate));
-    });
-
-    return {
-        getSelected: function () {
-            return element.val();
-        }
-    };
+    api.getData(dataset, callback);
 }
 
 
 function buildLayerList(element) {
+    var callback;
     var selected = 'norges_grunnkart_graatone';
 
     var options = _.chain(L.tileLayer.kartverket.getLayers())
@@ -47,90 +105,80 @@ function buildLayerList(element) {
         .map(layerTemplate)
         .value();
 
+
     element.html(options);
 
+    element.on('change', function (e) {
+        callback();
+    });
+
     return {
-        getSelected: function () {
-            return element.val();
+        getValues: function () {
+            return {layer: element.val()};
+        },
+        callback: function (cb) {
+            callback = cb;
+            cb();
         }
     };
 }
 
 function buildDatasetList(element) {
-
+    var callback;
     var datasetConfig = KR.Config.getDatasetList(api, null);
-    var radios = _.map(datasetConfig, function (value, key) {
+    var checkboxes = _.map(datasetConfig, function (value, key) {
         var name = value.name || key;
         return datasetTemplate({name: name, key: key});
     });
 
-    element.html(radios);
+    element.html(checkboxes);
+
+    _.each(element.find('input'), function (checkbox) {
+        $(checkbox).on('change', function () {
+            callback();
+        });
+    })
 
     return {
-        getSelected: function () {
-            return element.find('input[type=checkbox]:checked').map(function () {
+        getValues: function () {
+            var checked = element.find('input[type=checkbox]:checked').map(function () {
                 return this.value;
             }).get();
+            console.log(checked);
+            return {
+                datasets: checked.join(',')
+            };
+        },
+        callback: function (cb) {
+            callback = cb;
+            cb();
         }
     };
 }
 
 
-function setupClick(element, municipalities, datasets, layer) {
+function setupClick(element, limits, layer, datasets) {
 
-    var lineElement = $('#line');
-    lineElement.keyup(function () {
-
-        if (lineElement.val() !== '') {
-            $('#municipalities').attr('disabled', 'disabled');
-        } else {
-            $('#municipalities').removeAttr('disabled');
-        }
-    });
-
-    element.on('click', function () {
-        var selectedDatasets = datasets.getSelected().join(',');
-
-        if (!selectedDatasets.length) {
-            $('#link').html('');
-            return;
-        }
-
-        var params = {
-            layer: layer.getSelected(),
-            datasets: selectedDatasets
-        };
-
-        var line = lineElement.val();
-        if (line !== '') {
-            params.line = line;
-            params.allstatic = true;
-
-
-
-            var buffer = $('#buffer').val();
-            if (buffer !== '') {
-                params.buffer = buffer;
-            }
-
-            var linecolor = $('#linecolor').val();
-            if (buffer === '') {
-                linecolor = '#0033ff';
-            }
-            params.linecolor = linecolor;
-        } else {
-            params.komm =  municipalities.getSelected();
-        }
-
-        params = KR.Util.createQueryParameterString(params);
+    var generateUrl = function () {
+        var params = _.extend({}, limits.getValues(), layer.getValues(), datasets.getValues());
         var path = window.location.pathname;
-        var url = location.protocol + '//' + location.host + path.replace('/generator.html', '') +  '/config.html?' + params;
-
+        var url = location.protocol + '//' + location.host + path.replace('/generator.html', '') +  '/config.html?' + KR.Util.createQueryParameterString(params);
         $('#link').html('<a href="' + url + '" target="_blank">' + url + '</a>');
-    });
+    };
+
+
+    limits.callback(generateUrl);
+    layer.callback(generateUrl);
+    datasets.callback(generateUrl);
+
+    element.on('click', generateUrl);
 }
 
-var municipalities = buildMunicipalityList($('#municipalities'));
-var datasets = buildDatasetList($('#datasets'));
-var layer = buildLayerList($('#layers'));
-setupClick($('#generate'), municipalities, datasets, layer);
+
+getMunicipalityList(function (municipalities) {
+    var limits = buildLimitSelections(['municipality', 'line', 'bbox'], municipalities);
+
+    var datasets = buildDatasetList($('#datasets'));
+    var layer = buildLayerList($('#layers'));
+    setupClick($('#generate'), limits, layer, datasets);
+});
