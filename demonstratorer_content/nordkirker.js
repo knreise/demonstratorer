5f95e89a936450e3ac88c34262df4f340010ad18
Vemund Olstad <vemund.olstad@ra.no>
2016-04-26 13:01:08 +0200

    Oppdatering av sparql-query for Kirker i Norden

--- demonstratorer_content/nordkirker.js
+++ demonstratorer_content/nordkirker.js
@@ -10,27 +10,28 @@
 		'   rdfs:label ?name ;' +
 		' <https://data.kulturminne.no/askeladden/schema/lokalitetskategori> ?loccat ;' +
 		' <https://data.kulturminne.no/askeladden/schema/lokalitetsart> ?locart ;' +
-		' <https://data.kulturminne.no/askeladden/schema/AnsvarligOrganisasjon> ?org ;' +
-		' <https://data.kulturminne.no/askeladden/schema/i-kommune> ?kommune  ;' +
-		' <https://data.kulturminne.no/askeladden/schema/geo/point/etrs89> ?point .' +
+		' <https://data.kulturminne.no/askeladden/schema/ansvarligorganisasjon> ?org ;' +
+		' <https://data.kulturminne.no/askeladden/schema/kommune> ?kommune  .' +
+		' optional { ?id <https://data.kulturminne.no/askeladden/schema/geo/point/etrs89> ?point .}' +
 		' optional { ?id <https://data.kulturminne.no/askeladden/schema/beskrivelse> ?description .}' +
 		' optional { ?loccat rdfs:label ?loccatlabel .}' +
-		' optional {  ?locart rdfs:label ?locartlabel .}' +
-		' optional {   ?org rdfs:label ?orglabel .}' +
+		' optional { ?locart rdfs:label ?locartlabel .}' +
+		' optional { ?org rdfs:label ?orglabel .}' +
 		' BIND(REPLACE(STR(?id), "https://data.kulturminne.no/askeladden/lokalitet/", "") AS ?lokid)' +
 		' BIND(bif:concat("http://www.kulturminnesok.no/kulturminnesok/kulturminne/?LOK_ID=", ?lokid) AS ?link)' +
 		' optional {' +
 		' {select sample(?picture) as ?picture ?id where {?picture <https://data.kulturminne.no/bildearkivet/schema/lokalitet> ?id}}' +
 		' ?picture <https://data.kulturminne.no/bildearkivet/schema/lokalitet> ?id .' +
+		' ?picture <https://data.kulturminne.no/bildearkivet/schema/license> ?picturelicence .' +
 		' ?picture <https://data.kulturminne.no/schema/source-link> ?imglink .' +
 		' ?picture rdfs:label ?picturelabel .' +
-		' ?picture dc:description ?picturedescription .' +
-		' ?picture <https://data.kulturminne.no/bildearkivet/schema/license> ?picturelicence .' +
+		' optional {?picture dc:description ?picturedescription .}' +
+		
 		' BIND(REPLACE(STR(?imglink), "http://kulturminnebilder.ra.no/fotoweb/default.fwx\\\\?search\\\\=", "") AS ?linkid)' +
 		' BIND(bif:concat("http://kulturminnebilder.ra.no/fotoweb/cmdrequest/rest/PreviewAgent.fwx?ar=5001&sz=600&rs=0&pg=0&sr=", ?linkid) AS ?img)' +
 		' BIND(bif:concat("http://kulturminnebilder.ra.no/fotoweb/cmdrequest/rest/PreviewAgent.fwx?ar=5001&sz=600&rs=0&pg=0&sr=", ?linkid) AS ?thumbnail)' +
 		'   }' +
-		' ?enk <https://data.kulturminne.no/askeladden/schema/i-lokalitet> ?id;' +
+		' ?enk <https://data.kulturminne.no/askeladden/schema/lokalitet> ?id;' +
 		'      <https://data.kulturminne.no/askeladden/schema/enkeltminneart> ?art ;' +
 		'         <https://data.kulturminne.no/askeladden/schema/datering> ?datering;' +
 		'         <https://data.kulturminne.no/askeladden/schema/vernetype> ?vernetype' +
