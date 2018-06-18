e107318a37d02e1eb48cbd0f47756534ef9e32e2
Atle Frenvik Sveen <atlsve@norkart.no>
2018-06-18 10:03:22 +0200

    better error handling

--- src/components/DatasetLoader/ApiLoader.js
+++ src/components/DatasetLoader/ApiLoader.js
@@ -18,13 +18,17 @@ export default function ApiLoader(api, flattenedDatasets) {
         var tileBounds = getTiles(bbox, dataset.minZoom);
 
         var res = [];
+        var errors = [];
         var finished = _.after(tileBounds.length, function () {
             var features = createFeatureCollection(_.flatten(_.map(res, r=> r.features)));
-            callback(null, filter(L.latLngBounds.fromBBoxString(bbox), features));
+            if (errors.length) {
+                callback(errors);
+            } else {
+                callback(null, filter(L.latLngBounds.fromBBoxString(bbox), features));
+            }
         });
 
         _.each(tileBounds, function (tileBound) {
-
             var fromCache = cache.get(datasetId, tileBound);
             if (fromCache) {
                 res.push(fromCache);
@@ -35,12 +39,12 @@ export default function ApiLoader(api, flattenedDatasets) {
                     tileBound,
                     function (data) {
                         cache.set(datasetId, tileBound, data);
-                        //callback(null, data);
                         res.push(data);
                         finished();
                     },
                     function (error) {
-                        callback(error, null);
+                        errors.push(error);
+                        finished();
                     }
                 );
             }
@@ -49,7 +53,6 @@ export default function ApiLoader(api, flattenedDatasets) {
 
     return function (datasetId, requestedBbox, callback) {
         var dataset = flattenedDatasets[datasetId];
-
         try {
             if (dataset.bbox || (dataset.isStatic && dataset.fixedBbox)) {
                 loadBbox(datasetId, dataset, requestedBbox, callback);
